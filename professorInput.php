494134d659fa2fa7067b8d664e7881bf266f6c6e
Duy Nguyen <46938160+duy301199@users.noreply.github.com>
2023-05-08 01:15:11 -0700

    Create professorInput.php

---
+++ professorInput.php
@@ -0,0 +1,16 @@
+<html>
+<body>
+<form action="professorInfo.php" method="POST">
+<p>Enter the social security number:</p>
+<input type="text" name="sno"/>
+<p><input type="submit" value="Search"/></p>
+</form>
+<p>OR</p>
+<form action="professorInfo2.php" method="POST">
+<p>Enter the course number and section number:</p>
+<p>Course number: <input type="text" name="icn"/></p>
+<p>Section number: <input type="text" name="isn"/></p>
+<p><input type="submit" value="Search"/></p>
+</form>
+</body>
+</html>
