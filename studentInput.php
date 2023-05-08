eb78875ff26b933d5b1631dcccc22ca83c16a428
Duy Nguyen <46938160+duy301199@users.noreply.github.com>
2023-05-08 01:16:55 -0700

    Create studentInput.php

---
+++ studentInput.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+<form action="studentInfo.php" method="POST">
+<p>Enter the course number:</p>
+<input type="text" name="icn"/>
+<input type="submit" value="Search"/></p>
+</form>
+<p>OR</p>
+<form action="studentInfo2.php" method="POST">
+<p>Enter the student CWID:</p>
+<input type="text" name="sid"/>
+<input type="submit" value="Search"/></p>
+</form>
+</body>
+</html>
